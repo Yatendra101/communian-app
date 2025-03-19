b533f331108ed5386862f5494c994e681072e1d2
Yatendra101 <yatendras0@gmail.com>
2025-03-19 14:05:16 +0530

    Update Login.js

--- communian-app/src/pages/Login.js
+++ communian-app/src/pages/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", { email, password });
+      const res = await axios.post(`${process.env.REACT_APP_API_URL}/auth/login`, { email, password });
       login(res.data.token);
       navigate("/events");
     } catch (error) {
@@ -33,8 +33,8 @@ const Login = () => {
 
 const styles = {
   Log: {
-    marginTop : "100px",
-  }
-}
+    marginTop: "100px",
+  },
+};
 
 export default Login;
