ad47834ceb6d5c95e2632742f87fea3a8376aa35
Yatendra101 <yatendras0@gmail.com>
2025-03-19 12:36:03 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,7 @@
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
+dotenv.config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
